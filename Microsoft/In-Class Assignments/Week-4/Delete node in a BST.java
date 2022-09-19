2133418a1cd3b4c13365ff9e98396f1c1d83cd0f
Menaga-9802 <107313942+Menaga-9802@users.noreply.github.com>
2022-09-19 21:23:03 +0530

    Create Delete node in a BST.java

---
+++ Microsoft/In-Class Assignments/Week-4/Delete node in a BST.java
@@ -0,0 +1,55 @@
+class Solution {
+
+public int max(TreeNode root){
+
+if(root.right!=null){
+
+return max(root.right);
+
+}return root.val;
+
+}
+
+public TreeNode deleteNode(TreeNode root, int key) {
+
+if(root==null) return null;
+
+if(root.val>key){
+
+root.left = deleteNode(root.left, key);
+
+}else if(root.val<key){
+
+root.right = deleteNode(root.right, key);
+
+}else{
+
+if(root.left!=null && root.right!=null){
+
+int lmax = max(root.left);
+
+root.val = lmax;
+
+root.left = deleteNode(root.left, lmax);
+
+}else if(root.left!=null){
+
+return root.left;
+
+}else if(root.right!=null){
+
+return root.right;
+
+}else{
+
+return null;
+
+}
+
+}
+
+return root;
+
+}
+
+}
