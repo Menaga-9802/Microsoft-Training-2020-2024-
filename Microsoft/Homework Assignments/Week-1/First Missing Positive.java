b2bbb0f6b1beaff7134dcc28116ddc7652302246
Menaga-9802 <107313942+Menaga-9802@users.noreply.github.com>
2022-09-22 12:16:09 +0530

    Update First Missing Positive.java

--- Microsoft/Homework Assignments/Week-1/First Missing Positive.java
+++ Microsoft/Homework Assignments/Week-1/First Missing Positive.java
@@ -1,25 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int i = 0;
-        while(i < nums.length){
-            int correct = nums[i] - 1;
-            if(nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correct]){
-                swap(nums, i , correct);
-            } else {
+        Arrays.sort(nums);
+        int k = Arrays.binarySearch(nums, 0);
+        int i = 1, j = 0;
+
+        if (k < -1)
+            k = -k - 2;
+
+        while (k + i <= nums.length - 1) {
+            if (nums[k + i] == j)
                 i++;
+            else if (nums[k+i]==j+1) {
+                i++;
+                j++;
             }
-        }
-        for(int j = 0; j < nums.length; j++) {
-            if(nums[j] != j + 1) {
+            else if (nums[k+i]>j+1)
                 return j+1;
-            }
         }
-        return i + 1;
-    }
-    
-    static void swap(int[] arr , int i , int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        return j+1;
     }
 }
