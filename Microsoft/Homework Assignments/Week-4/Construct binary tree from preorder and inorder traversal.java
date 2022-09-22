a23ae2d3eb644a82f0cbb803554cd5dfe75b66d5
Menaga-9802 <107313942+Menaga-9802@users.noreply.github.com>
2022-09-22 14:50:29 +0530

    Create Construct binary tree from preorder and inorder traversal.java

---
+++ Microsoft/Homework Assignments/Week-4/Construct binary tree from preorder and inorder traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    int preIndex = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        TreeNode curr = cTree(preorder, inorder, 0, preorder.length - 1);
+        return curr;
+    }
+    public TreeNode cTree(int[] preorder, int[] inorder, int is, int ie){
+        if(is > ie) return null;
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+        int inIndex = 0;
+        for(int i = is; i <= ie; i++){
+            if(inorder[i] == root.val){
+                inIndex = i;
+                break;
+            }
+        }
+        root.left = cTree(preorder, inorder, is, inIndex - 1);
+        root.right = cTree(preorder, inorder, inIndex + 1, ie);
+        return root;
+    }
+}
