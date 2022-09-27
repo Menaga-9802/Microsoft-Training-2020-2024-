fd3e4f1ebd17b52f0159cb63773938b1b42814f8
Menaga-9802 <107313942+Menaga-9802@users.noreply.github.com>
2022-09-27 12:37:41 +0530

    Create Swap nodes in pairs.java

---
+++ Microsoft/Midterm Assessments/Swap nodes in pairs.java
@@ -0,0 +1,20 @@
+class Solution {
+   public ListNode swapPairs(ListNode head) {
+   if(head == null  || head.next == null){
+     return head;
+   }
+   ListNode curr = head.next.next;
+   ListNode prev = head;
+   head.next.next = head;
+   head = head.next;
+   while(curr != null && curr.next != null){
+        prev.next  = curr.next;
+        prev = curr;
+        ListNode temp = curr.next.next;
+        curr.next.next = curr;
+        curr = temp;
+   }
+   prev.next = curr;
+   return head;
+   }
+}
