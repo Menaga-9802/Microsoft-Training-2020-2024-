02e2754a3e755b7055a64f22113095828f90cbf7
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-10-20 15:19:06 +0530

    Create K th largest element in a stream.java

---
+++ Microsoft/Homework Assignments/Week-7/K th largest element in a stream.java
@@ -0,0 +1,17 @@
+class KthLargest {
+    PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+    int k;  
+    public KthLargest(int k, int[] nums) {
+        this.k = k; 
+        for(int a: nums){
+            pq.add(a); 
+        }
+    }
+     public int add(int val) {
+        pq.add(val);  
+        while(pq.size()>k){
+            pq.remove(); 
+        }
+        return pq.peek();
+    }
+}
