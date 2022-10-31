7144522ecfc01a0c2bcfb5f1cc4a0d81420b9018
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-10-31 19:48:06 +0530

    Create Best time to buy and sell stock.java

---
+++ Microsoft/Assignment Problems(77)/Best time to buy and sell stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      int buy=Integer.MAX_VALUE;
+      int profit=0;
+      for(int i=0;i<prices.length;i++){
+        if(prices[i]<buy){
+            buy=prices[i];
+        }
+        else{
+            int diff=prices[i]-buy;
+            profit=Math.max(diff, profit);
+        }
+      }
+    return profit;
+    }
+}
