0271e9c2c531dd2e08866b53d891e9f8117c2bc1
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-01 15:52:13 +0530

    Create 36. Reorganize String.java

---
+++ Microsoft/Question bank/Amazon/36. Reorganize String.java
@@ -0,0 +1,28 @@
+class Solution {
+public String reorganizeString(String s) {
+    int n = s.length() , limit = n % 2 == 0 ? n / 2 : (n + 1) / 2;
+    int[] freqMap = new int[26];
+    s.chars().forEach(i -> freqMap[i-97]++);
+    PriorityQueue<Character> queue = new PriorityQueue<>((a,b) ->  freqMap[b - 97] - freqMap[a - 97]);
+    for(int i = 0 ; i < 26 ; i++){
+        if(freqMap[i] > 0){
+            if(freqMap[i] > limit) return "";
+            queue.add((char)(i + 97));
+        }    
+    }
+    StringBuilder res = new StringBuilder();
+    while(queue.size() > 1){
+        char first = queue.poll();
+        char second = queue.poll();
+        res.append(first).append(second);
+        if(--freqMap[first - 97] > 0) queue.add(first);
+        if(--freqMap[second - 97] > 0) queue.add(second);
+    }
+    if(!queue.isEmpty()){
+        char last = queue.poll();
+        if(freqMap[last - 97] > 1) return "";
+        res.append(last);
+    }
+     return res.toString();
+}
+}
