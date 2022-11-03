916c3a9ff0579b13857e86adf28e089620792d1a
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-03 15:15:17 +0530

    Update 42. Generate Parentheses.java

--- Microsoft/Question bank/Google/42. Generate Parentheses.java
+++ Microsoft/Question bank/Google/42. Generate Parentheses.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new LinkedList<>();
+        generatePar(n,n,list,new String(""));
+        return list;
+    }
+     public void generatePar(int p1,int p2,List<String> list,String s){   
+        if(p1 == p2){          
+            s += '(';
+            p1--;
+        }
+        if(p1 == 0 && p2 > 0){ 
+            while(p2 != 0){
+                s += ')';
+                p2--;
+            }
+            list.add(s);
+            return;
+        }
+        generatePar(p1-1,p2,list,s+'(');    
+        generatePar(p1,p2-1,list,s+')');   
+    }
+}
