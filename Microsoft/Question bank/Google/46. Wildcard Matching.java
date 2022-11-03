873a070f8f92a2ca0c75d8532370298168c9770a
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-03 15:24:59 +0530

    Update 46. Wildcard Matching.java

--- Microsoft/Question bank/Google/46. Wildcard Matching.java
+++ Microsoft/Question bank/Google/46. Wildcard Matching.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public boolean isMatch(String b, String a) {
+        int n = a.length();
+        int m = b.length();
+        boolean prev[] = new boolean[m+1];
+        prev[0] = true;
+        for(int i=1;i<=n;i++){
+            boolean curr[] = new boolean[m+1];
+            curr[0] = a.charAt(i-1)=='*' && prev[0];
+            for(int j=1;j<=m;j++){
+                if(a.charAt(i-1)==b.charAt(j-1) || a.charAt(i-1)=='?') curr[j] = prev[j-1];
+                else if(a.charAt(i-1)=='*') curr[j] = prev[j] || curr[j-1];
+                else curr[j] = false;
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+}
