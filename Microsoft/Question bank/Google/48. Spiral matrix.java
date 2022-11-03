c7fa5337e67cdf34c64f15b076ddfc3e45308954
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-03 15:30:36 +0530

    Update 48. Spiral matrix.java

--- Microsoft/Question bank/Google/48. Spiral matrix.java
+++ Microsoft/Question bank/Google/48. Spiral matrix.java
@@ -1,1 +1,35 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if(matrix.length == 0)
+            return res;
+        int topRow = 0;
+        int bottomRow = matrix.length - 1;
+        int leftCol = 0;
+        int rightCol = matrix[0].length - 1;
+        while(topRow <= bottomRow && leftCol <= rightCol){
+            for(int i=leftCol; i<=rightCol; i++){
+                res.add(matrix[topRow][i]);   
+            }
+            topRow++;
+            for(int i=topRow; i<=bottomRow; i++){
+                res.add(matrix[i][rightCol]);
+            }
+            rightCol--;
+            if(topRow <= bottomRow){
+                for(int i=rightCol; i>=leftCol; i--){
+                    res.add(matrix[bottomRow][i]);
+                }
+            }
+            bottomRow--;
+            if(leftCol <= rightCol){
+                for(int i=bottomRow; i>=topRow; i--){
+                    res.add(matrix[i][leftCol]);
+                }
+            }
+            leftCol++;
+         }
+         return res;
+    }
+}
 
