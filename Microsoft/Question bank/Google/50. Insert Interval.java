a5d337c29ab193a2e6e74772c0558b45308dcae9
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-03 15:33:53 +0530

    Update 50. Insert Interval.java

--- Microsoft/Question bank/Google/50. Insert Interval.java
+++ Microsoft/Question bank/Google/50. Insert Interval.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+    List<int[]> l = new ArrayList<>();
+    int i = 0, N = intervals.length;
+    while (i < N && intervals[i][1] < newInterval[0]) 
+        l.add(intervals[i++]);
+    while (i < N && intervals[i][0] <= newInterval[1]) { 
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i++][1]);
+    }
+    l.add(newInterval);       
+    while (i < N) l.add(intervals[i++]);
+    return l.toArray(new int[][]{});
+  }
+}
