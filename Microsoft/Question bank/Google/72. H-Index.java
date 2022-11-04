2790ce1ab4c57ca3afd6d04ca3292ead0b03e140
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-04 20:30:11 +0530

    Update 72. H-Index.java

--- Microsoft/Question bank/Google/72. H-Index.java
+++ Microsoft/Question bank/Google/72. H-Index.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        Arrays.sort(citations);
+        for(int i=0;i<citations.length;i++){
+            if(citations[i]>=n){
+                return n;
+            }
+            n--;
+        }
+        return 0;
+    }
+}
