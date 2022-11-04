83b119f1fc7f94bbf76ce94074773e7bc31cac22
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-04 10:01:08 +0530

    Update 57. Find Peak Element.java

--- Microsoft/Question bank/Google/57. Find Peak Element.java
+++ Microsoft/Question bank/Google/57. Find Peak Element.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    public int findPeakElement(int[] nums){
+        if (nums.length == 1) {
+            return 0;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high - 1) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            else if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        if (nums[low] < nums[high]) {
+            return high;
+        }
+        else {
+            return low;
+        }
+    }
+}
