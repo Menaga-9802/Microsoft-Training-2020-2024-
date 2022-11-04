03492b4f4a4a116fdae18b5dacfea671d794c0f7
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-04 19:33:32 +0530

    Update 66. Summary ranges.java

--- Microsoft/Question bank/Google/66. Summary ranges.java
+++ Microsoft/Question bank/Google/66. Summary ranges.java
@@ -1,1 +1,24 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+		List<String> list = new ArrayList<>();
+		for (int i = 0, len = nums.length, k; i < len; i = k + 1) {
+			k = help(nums, i, len);
+			if (i != k)
+				list.add("" + nums[i] + "->" + nums[k]);
+			else
+				list.add("" + nums[i]);
+		}
+		return list;
+	}
 
+	private int help(int[] nums, int l, int r) {
+		while (l + 1 < r) {
+			int m = (l + r) / 2;
+			if (nums[m] - nums[l] == m - l)
+				l = m;
+			else
+				r = m;
+		}
+		return l;
+	}
+}
