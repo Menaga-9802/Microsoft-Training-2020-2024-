312202e6beedbb73ebac83db8ebb77c400af3164
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-04 09:27:19 +0530

    Update 53. Word break.java

--- Microsoft/Question bank/Google/53. Word break.java
+++ Microsoft/Question bank/Google/53. Word break.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean wordBreak(String sentence, List<String> dictionary) {
+        int[] dp = new int[sentence.length()];
+		if(dictionary.contains(sentence.substring(0, 1)))
+			dp[0] = 1;
+		
+		for(int i = 1; i < dp.length; i++) {
+			int ans = dictionary.contains(sentence.substring(0, i + 1)) == true ? 1 : 0;
+			for(int j = 0; j < i; j++) {
+				int val = dictionary.contains(sentence.subSequence(j + 1, i + 1)) == true ? 1 : 0;
+				ans += val * dp[j];
+			}
+			dp[i] = ans;
+		}
+		
+		return dp[sentence.length() - 1] > 0 ? true : false;
+    }
+}
