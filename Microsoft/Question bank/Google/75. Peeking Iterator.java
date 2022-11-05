978184716074f7ea015a3f1ff401a25ac3fcd134
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2022-11-05 09:24:48 +0530

    Update 75. Peeking Iterator.java

--- Microsoft/Question bank/Google/75. Peeking Iterator.java
+++ Microsoft/Question bank/Google/75. Peeking Iterator.java
@@ -1,1 +1,27 @@
-
+class PeekingIterator implements Iterator<Integer> {
+    Iterator<Integer> iterator;
+    Integer peek;
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    this.iterator=iterator;
+        if(iterator.hasNext()){
+            peek=iterator.next();
+        }
+	}
+	public Integer peek() {
+        return peek;
+	}
+	@Override
+	public Integer next() {
+	    Integer next = peek;
+        if(iterator.hasNext()){
+            peek=iterator.next();
+        }else{
+            peek=null;
+        }
+        return next;
+	}
+	@Override
+	public boolean hasNext() {
+	    return peek!=null;
+	}
+}
